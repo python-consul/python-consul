d5f6577e02aacf876e0359ddc500d53f87a7dc9b
Andy Gayton <andy@thecablelounge.com>
2014-09-30 11:42:10 -0700

    the lib takes care of unbase64-ing the value

--- consul/base.py
+++ consul/base.py
@@ -43,9 +43,9 @@ class Consul(object):
                     "CreateIndex": 100,
                     "ModifyIndex": 200,
                     "LockIndex": 200,
-                    "Key": "zip",
+                    "Key": "foo",
                     "Flags": 0,
-                    "Value": "dGVzdA==",
+                    "Value": "bar",
                     "Session": "adf4238a-882b-9ddc-4a9d-5b6758e4159e"
                 }
             """

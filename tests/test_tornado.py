c89bde317f676974f2b3e0bd61508c83757838e5
Andy Gayton <agayton@livefyre.com>
2014-10-29 15:34:43 -0700

    check ACLs work OK from tornado client

--- tests/test_tornado.py
+++ tests/test_tornado.py
@@ -5,6 +5,7 @@ from tornado import gen
 
 import pytest
 
+import consul
 import consul.tornado
 
 
@@ -215,3 +216,30 @@ class TestConsul(object):
 
         loop.add_callback(monitor)
         loop.run_sync(keepalive)
+
+    def test_acl(self, loop, acl_consul):
+        @gen.coroutine
+        def main():
+            c = consul.tornado.Consul(
+                port=acl_consul.port, token=acl_consul.token)
+
+            rules = """
+                key "" {
+                    policy = "read"
+                }
+                key "private/" {
+                    policy = "deny"
+                }
+            """
+            token = yield c.acl.create(rules=rules)
+
+            try:
+                yield c.acl.list(token=token)
+            except consul.ACLPermissionDenied:
+                raised = True
+            assert raised
+
+            destroyed = yield c.acl.destroy(token)
+            assert destroyed is True
+            loop.stop()
+        loop.run_sync(main)

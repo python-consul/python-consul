1692e59072fcee64c08ef8932980a582a9e14d65
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2016-05-01 17:46:11 +1000

    twisted: add twisted pacakge to extras require

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
     extras_require={
         'tornado': ['tornado'],
         'asyncio': ['aiohttp'],
+        'twisted': ['twisted'],
     },
     tests_require=['pytest'],
     cmdclass={'test': PyTest,

f4eeb3676d2efd5e0006c06f76f84c0186800fc5
lian <draggili@gmail.com>
2017-12-16 17:20:52 +0100

    set timeout when making http requests

--- consul/std.py
+++ consul/std.py
@@ -18,22 +18,26 @@ class HTTPClient(base.HTTPClient):
 
     def get(self, callback, path, params=None):
         uri = self.uri(path, params)
+        params.timeout = self.timeout
         return callback(self.response(
             self.session.get(uri, verify=self.verify, cert=self.cert)))
 
     def put(self, callback, path, params=None, data=''):
         uri = self.uri(path, params)
+        params.timeout = self.timeout
         return callback(self.response(
             self.session.put(uri, data=data, verify=self.verify,
                              cert=self.cert)))
 
     def delete(self, callback, path, params=None):
         uri = self.uri(path, params)
+        params.timeout = self.timeout
         return callback(self.response(
             self.session.delete(uri, verify=self.verify, cert=self.cert)))
 
     def post(self, callback, path, params=None, data=''):
         uri = self.uri(path, params)
+        params.timeout = self.timeout
         return callback(self.response(
             self.session.post(uri, data=data, verify=self.verify,
                               cert=self.cert)))

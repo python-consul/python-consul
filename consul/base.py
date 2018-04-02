f80efefea958fcc51e64e09520e2e2d19018a1c9
Aaron Tolman <tolman-aaron@zai.com>
2018-04-02 10:27:39 -0400

    Missed a reference to Nomad

--- consul/base.py
+++ consul/base.py
@@ -223,7 +223,7 @@ class CB(object):
                 if map:
                     data = map(data)
             if index:
-                return response.headers['X-Nomad-Index'], data
+                return response.headers['X-Consul-Index'], data
             return data
         return cb
 

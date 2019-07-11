8c03c0fa4ee645c1283166d4785f44f81dc3ed09
Shaheed Haque <srhaque@theiet.org>
2019-07-11 12:36:25 +0100

    Port away from requests -> urllib3 for thread-safety. Resolves #144.

--- consul/std.py
+++ consul/std.py
@@ -1,42 +1,42 @@
-import requests
+import urllib3
 
 from consul import base
 
 
 __all__ = ['Consul']
+JSON_HEADER = {'Content-Type': 'application/json'}
 
 
 class HTTPClient(base.HTTPClient):
     def __init__(self, *args, **kwargs):
         super(HTTPClient, self).__init__(*args, **kwargs)
-        self.session = requests.session()
+        cert_file = kwargs.get('cert', None)
+        cert_reqs = 'CERT_REQUIRED' if kwargs.get('verify', False) else None
+        self.session = urllib3.PoolManager(cert_file=cert_file, cert_reqs=cert_reqs)
 
     def response(self, response):
-        response.encoding = 'utf-8'
         return base.Response(
-            response.status_code, response.headers, response.text)
+            response.status, response.headers, response.data.decode('utf-8'))
 
     def get(self, callback, path, params=None):
         uri = self.uri(path, params)
         return callback(self.response(
-            self.session.get(uri, verify=self.verify, cert=self.cert)))
+            self.session.request('GET', uri)))
 
     def put(self, callback, path, params=None, data=''):
         uri = self.uri(path, params)
         return callback(self.response(
-            self.session.put(uri, data=data, verify=self.verify,
-                             cert=self.cert)))
+            self.session.request('PUT', uri, body=data, headers=JSON_HEADER)))
 
     def delete(self, callback, path, params=None):
         uri = self.uri(path, params)
         return callback(self.response(
-            self.session.delete(uri, verify=self.verify, cert=self.cert)))
+            self.session.request('DELETE', uri)))
 
     def post(self, callback, path, params=None, data=''):
         uri = self.uri(path, params)
         return callback(self.response(
-            self.session.post(uri, data=data, verify=self.verify,
-                              cert=self.cert)))
+            self.session.request('POST', uri, body=data, headers=JSON_HEADER)))
 
 
 class Consul(base.Consul):
